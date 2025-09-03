23e03800cb0d5043c3134528d9100aeffe19c974
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2025-09-03 10:37:11 -0500

    Add azumarill base stats and gfx

--- data/pokemon/base_stats/azumarill.asm
+++ data/pokemon/base_stats/azumarill.asm
@@ -1,17 +1,17 @@
-	db MARILL ; 183
+	db AZUMARILL ; 184
 
-	db  70,  20,  50,  40,  20,  50
+	db 100,  50,  80,  50,  50,  80
 	;   hp  atk  def  spd  sat  sdf
 
 	db WATER, WATER ; type
-	db 190 ; catch rate
-	db 58 ; base exp
+	db 75 ; catch rate
+	db 153 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/marill/front.dimensions"
+	INCBIN "gfx/pokemon/azumarill/front.dimensions"
 	db 0, 0, 0, 0 ; padding
 	db GROWTH_FAST ; growth rate
 	dn EGG_WATER_1, EGG_FAIRY ; egg groups
