f571e25da3791b685456a1ceaad13cce462de0ec
etdv-thevoid <9028176+etdv-thevoid@users.noreply.github.com>
2025-09-12 10:43:17 -0500

    Increase levels of headbutt/rocksmash mons to reflect delayed aquisition of moves

--- data/wild/treemons.asm
+++ data/wild/treemons.asm
@@ -16,131 +16,135 @@ TreeMons:
 
 TreeMonSet_City:
 ; common
-	db 50, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, SPEAROW,    15
+	db 15, SPEAROW,    15
+	db 15, SPEAROW,    15
+	db 10, AIPOM,      15
+	db  5, AIPOM,      15
+	db  5, AIPOM,      15
 	db -1
 ; rare
-	db 50, SPEAROW,    10
-	db 15, SCYTHER,  10
-	db 15, SCYTHER,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, SPEAROW,    15
+	db 15, SCYTHER,    15
+	db 15, SCYTHER,    15
+	db 10, AIPOM,      15
+	db  5, AIPOM,      15
+	db  5, AIPOM,      15
 	db -1
 
 TreeMonSet_Urban:
 ; common
-	db 50, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, SPEAROW,    15
+	db 15, SPEAROW,    15
+	db 15, SPEAROW,    15
+	db 10, AIPOM,      15
+	db  5, AIPOM,      15
+	db  5, AIPOM,      15
 	db -1
 ; rare
-	db 50, SPEAROW,    10
-	db 15, SCYTHER,  10
-	db 15, SCYTHER,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, SPEAROW,    15
+	db 15, SCYTHER,    15
+	db 15, SCYTHER,    15
+	db 10, AIPOM,      15
+	db  5, AIPOM,      15
+	db  5, AIPOM,      15
 	db -1
 
 TreeMonSet_Town:
 ; common
-	db 50, SPEAROW,    10
-	db 15, EKANS,      10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, SPEAROW,    15
+	db 15, EKANS,      15
+	db 15, SPEAROW,    15
+	db 10, AIPOM,      15
+	db  5, AIPOM,      15
+	db  5, AIPOM,      15
 	db -1
 ; rare
-	db 50, SPEAROW,    10
-	db 15, PINSIR,  10
-	db 15, PINSIR,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, SPEAROW,    15
+	db 15, PINSIR,     15
+	db 15, PINSIR,     15
+	db 10, AIPOM,      15
+	db  5, AIPOM,      15
+	db  5, AIPOM,      15
 	db -1
 
 TreeMonSet_Overworld1:
 ; common
-	db 50, HOOTHOOT,   10
-	db 15, SPINARAK,   10
-	db 15, LEDYBA,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, HOOTHOOT,   15
+	db 15, SPINARAK,   15
+	db 15, LEDYBA,     15
+	db 10, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
-	db 15, NATU,     10
-	db 15, NATU,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, HOOTHOOT,   15
+	db 15, NATU,       15
+	db 15, NATU,       15
+	db 10, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
 	db -1
 
 TreeMonSet_Overworld3:
 ; common
-	db 50, HOOTHOOT,   10
-	db 15, EKANS,      10
-	db 15, HOOTHOOT,   10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, HOOTHOOT,   15
+	db 15, EKANS,      15
+	db 15, HOOTHOOT,   15
+	db 10, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
-	db 15, NATU,     10
-	db 15, NATU,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, HOOTHOOT,   15
+	db 15, NATU,       15
+	db 15, NATU,       15
+	db 10, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
 	db -1
 
 TreeMonSet_Overworld2:
 ; common
-	db 50, HOOTHOOT,   10
-	db 15, VENONAT,    10
-	db 15, HOOTHOOT,   10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, HOOTHOOT,   15
+	db 15, VENONAT,    15
+	db 15, HOOTHOOT,   15
+	db 10, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
-	db 15, NATU,     10
-	db 15, NATU,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, HOOTHOOT,   15
+	db 15, NATU,       15
+	db 15, NATU,       15
+	db 10, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
+	db  5, EXEGGCUTE,  15
 	db -1
 
 TreeMonSet_Forest:
 ; common
-	db 50, HOOTHOOT,   10
-	db 15, NATU,     10
-	db 15, NATU,     10
-	db 10, NOCTOWL,    10
-	db  5, BUTTERFREE, 10
-	db  5, BEEDRILL,   10
+	db 50, HOOTHOOT,   15
+	db 15, NATU,       15
+	db 15, NATU,       15
+	db 10, NOCTOWL,    15
+	db  5, BUTTERFREE, 15
+	db  5, BEEDRILL,   15
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
-	db 15, CATERPIE,   10
-	db 15, WEEDLE,     10
-	db 10, HOOTHOOT,   10
-	db  5, METAPOD,    10
-	db  5, KAKUNA,     10
+	db 50, HOOTHOOT,   15
+	db 15, CATERPIE,   15
+	db 15, WEEDLE,     15
+	db 10, HOOTHOOT,   15
+	db  5, METAPOD,    15
+	db  5, KAKUNA,     15
 	db -1
 
 TreeMonSet_Rock:
-	db 90, KRABBY,     15
-	db 10, KRABBY,    15
+	db 50, KRABBY,     20
+	db 15, GEODUDE,    20
+	db 15, GEODUDE,    20
+	db 10, KRABBY,     20
+	db  5, DIGLETT,    20
+	db  5, DIGLETT,    20
 	db -1
